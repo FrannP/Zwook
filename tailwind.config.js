a79abb39c8cfda0b7aea55002d5f569e5dd3a384
Klinci <franpinter20@gmail.com>
2024-07-12 10:34:44 +0200

    add repo

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      /** Light Mode */
+      
+
+      /** Dark Mode */
+    },
+    extend: {},
+  },
+  plugins: [],
+}
