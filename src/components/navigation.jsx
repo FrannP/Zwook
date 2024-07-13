639f8455ad7b19ba6861051c73b5513c34e7c591
FrannP <franpinter20@gmail.com>
2024-07-13 18:09:09 +0200

    add premium plan alert

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -27,12 +27,40 @@ const NavItem = ({ icon, label, link, alert }) => (
   </Link>
 );
 
+const Bar = ({ color, backgroundColor, progress }) => (
+    <div className={`h-[8px] w-full rounded-[360px] ${backgroundColor}`}>
+      <div
+        className="h-full rounded-[360px]"
+        style={{ width: `${progress}%`, backgroundColor: color }}
+      ></div>
+    </div>
+  );
+
+const PlanAlert = () =>(
+    <div className="p-[16px] rounded-[16px] border-solid border-[1px] border-primaryBorder_Dark w-full flex flex-col gap-[20px]">
+        <div className="flex  items-center gap-[12px]">
+            <img src="src\assets\icons\premiumIcon.svg" alt="" />
+            <h3 className="font-medium text-lg">Update Premium</h3>
+        </div>
+
+        <p className="font-regular text-md text-primaryPlaceholderText_Light">
+            Your Premium account is coming to an end. Please update your credit card information.
+        </p>
+
+        <Bar color="bg-text-warningSurface_Dark" backgroundColor="bg-primaryActiveSurface_dark" progress={83} />
+        <div className="flex gap-[12px]">
+            <button className="font-regular text-md">Dismiss</button>
+            <button className="font-regular text-md text-warningText_Dark">Upgrade Plan</button>
+        </div>
+    </div>
+);
+
 export default function Navigation() {
   return (
-    <div className="w-[248px] h-full bg-secondaryBackground_Dark p-[24px]">
+    <div className="w-[296px] h-full flex flex-col justify-between bg-secondaryBackground_Dark p-[24px]">
       <div className="flex flex-col items-center gap-[24px] justify-center">
         <img
-          className="w-[186.63px] h-[52px]"
+          className="w-[248px] h-[76px]"
           src="src/assets/logo.svg"
           alt="Logo"
         />
@@ -47,6 +75,9 @@ export default function Navigation() {
           <NavItem icon={stationsIcon} label="Stations" link="/dashboard" />
           <NavItem icon={podcastsIcon} label="Podcasts" link="/dashboard" alert="Add New" />
         </ul>
+      </div>
+      <div>
+        <PlanAlert/>
       </div>
     </div>
   );
