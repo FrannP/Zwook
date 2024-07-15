3befd640f6ff99b9be512f4f09146e79cc16dc46
FrannP <franpinter20@gmail.com>
2024-07-15 12:14:17 +0200

    add active state to button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default function Button({label}) {
+export default function Button({ label }) {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <button className="h-[36px] p-[12px] text-center rounded-[8px] border-solid border-[1px] border-primaryBorder_Dark text-primaryText_Dark flex justify-center items-center">
+    <button
+      onClick={handleClick}
+      className={`h-[36px] p-[12px] text-center rounded-[8px] border-solid border-[1px] border-primaryBorder_Dark flex justify-center items-center transition duration-500 ease-in-out ${
+        isActive ? "bg-invertSurface_dark text-invertText_Dark" : "text-primaryText_Dark"
+      }`}
+    >
       {label}
     </button>
   );
