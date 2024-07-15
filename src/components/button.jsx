fa2fa97c7ab2f1109d31ef80ec41525408929892
FrannP <franpinter20@gmail.com>
2024-07-15 12:18:28 +0200

    revamp button function

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
+import React from 'react';
 
-export default function Button({ label }) {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
-
+export default function Button({ label, isActive, onClick }) {
   return (
     <button
-      onClick={handleClick}
+      onClick={onClick}
       className={`h-[36px] p-[12px] text-center rounded-[8px] border-solid border-[1px] border-primaryBorder_Dark flex justify-center items-center transition duration-500 ease-in-out ${
         isActive ? "bg-invertSurface_dark text-invertText_Dark" : "text-primaryText_Dark"
       }`}
--- src/components/dashboardSectionHeader.jsx
+++ src/components/dashboardSectionHeader.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./button";
 
 export default function DashboardContentHeader({ title, buttonLabels }) {
+  const [activeButtonIndex, setActiveButtonIndex] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setActiveButtonIndex(index === activeButtonIndex ? null : index);
+  };
+
   return (
     <div className="flex justify-between items-center py-[6px] px-[4px]">
       <h2 className="text-xlText font-semiBold">{title}</h2>
       <div className="flex gap-[12px]">
         {buttonLabels.map((label, index) => (
-          <Button key={index} label={label} />
+          <Button
+            key={index}
+            label={label}
+            isActive={index === activeButtonIndex}
+            onClick={() => handleButtonClick(index)}
+          />
         ))}
       </div>
     </div>
