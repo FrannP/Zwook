10f3940c53b3f2684512ed87fc5a7a2a147f98a6
FrannP <franpinter20@gmail.com>
2024-07-16 20:13:19 +0200

    add album creation form

--- src/components/addNewAlbumContent.jsx
+++ src/components/addNewAlbumContent.jsx
@@ -15,23 +15,73 @@ const Header = () => {
     <header className="flex justify-between">
       <h1 className="font-medium text-smDisplay">Create New Podcast</h1>
       <div className="flex gap-[12px]">
-        <Button label="Cancel" borderColor={"border-primaryBorder_Dark"} />
+        <Button label="Cancel" borderColor="border-primaryBorder_Dark" />
         <Button
           label="Save"
-          textColor={"text-darkText_Dark"}
-          borderColor={"border-warningSurface_Dark"}
-          backgroundColor={"bg-warningSurface_Dark"}
+          textColor="text-darkText_Dark"
+          borderColor="border-warningSurface_Dark"
+          backgroundColor="bg-warningSurface_Dark"
         />
       </div>
     </header>
   );
 };
 
+const TextInput = ({ label, placeholder }) => {
+  return (
+    <label className="flex flex-col gap-[8px]">
+      <span className="font-regular text-mdText">{label}</span>
+      <input type="text" placeholder={placeholder} className="border-[1px] border-gray-300 p-[8px] rounded-[4px]" />
+    </label>
+  );
+};
+
+const CheckboxInput = ({ label }) => {
+  return (
+    <label className="flex items-center gap-[8px]">
+      <input type="checkbox" className="form-checkbox" />
+      <span className="font-regular text-mdText">{label}</span>
+    </label>
+  );
+};
+
+const TextAreaInput = ({ label, placeholder }) => {
+  return (
+    <label className="flex flex-col gap-[8px]">
+      <span className="font-regular text-mdText">{label}</span>
+      <textarea  placeholder={placeholder} className="border-[1px] border-gray-300 p-[8px] rounded-[4px]" />
+    </label>
+  );
+};
+
+const Form = () => {
+  return (
+    <form className="flex flex-col gap-[20px]">
+      <TextInput label="Title" placeholder="Enter station title" />
+      <TextInput label="Description link" placeholder="Enter website URL" />
+      <TextAreaInput label="Description" placeholder="Enter station description"/>
+      <CheckboxInput label="Status" />
+      <CheckboxInput label="Premium" />
+      <CheckboxInput label="Make Default" />
+      <CheckboxInput label="Visible on the Portal" />
+    </form>
+  );
+};
+
+const FormSection = () => {
+  return (
+    <section className="flex gap-[20px]">
+      <Form />
+    </section>
+  );
+};
+
 export default function addNewAlbumContent() {
   return (
-    <div>
+    <div className="flex flex-col gap-[36px]">
       <Header />
-      <section></section>
+      <FormSection />
     </div>
   );
 }
+ 
