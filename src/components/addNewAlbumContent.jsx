f4f7b7be19c5b7c8da3213ed868a3e118eea313a
FrannP <franpinter20@gmail.com>
2024-07-16 14:00:08 +0200

    add header

--- src/components/addNewAlbumContent.jsx
+++ src/components/addNewAlbumContent.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import Button from "./button";
+
+const Button = ({ label, backgroundColor, textColor, borderColor }) => {
+  return (
+    <button
+      className={`py-[12px] px-[20px]  rounded-[8px] border-solid border-[1px] ${borderColor} ${backgroundColor} ${textColor} text-mdText font-regular`}
+    >
+      {label}
+    </button>
+  );
+};
 
 const Header = () => {
   return (
     <header className="flex justify-between">
-      <h1>Create New Podcast</h1>
-      <div>
-        <Button />
+      <h1 className="font-medium text-smDisplay">Create New Podcast</h1>
+      <div className="flex gap-[12px]">
+        <Button label="Cancel" borderColor={"border-primaryBorder_Dark"} />
+        <Button
+          label="Save"
+          textColor={"text-darkText_Dark"}
+          borderColor={"border-warningSurface_Dark"}
+          backgroundColor={"bg-warningSurface_Dark"}
+        />
       </div>
     </header>
   );
@@ -16,7 +31,7 @@ export default function addNewAlbumContent() {
   return (
     <div>
       <Header />
-      <section>assssssssssssssssssssss</section>
+      <section></section>
     </div>
   );
 }
