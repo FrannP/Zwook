4c4ebdfef75ad7fd45bcf02865bcb79920d7eee1
FrannP <franpinter20@gmail.com>
2024-07-24 14:16:53 +0200

    update images in podcastSection

--- src/components/podcastsSection.jsx
+++ src/components/podcastsSection.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
-import DashboardSectionHeader from './dashboardSectionHeader';
-import PodcastCard from './podcastCard';
-import podcastOne from '../assets/dashboardImages/podcastOne.svg';
+import React from "react";
+import DashboardSectionHeader from "./dashboardSectionHeader";
+import PodcastCard from "./podcastCard";
+import podcastOne from "../assets/dashboardImages/podcastOne.svg";
+import podcastTwo from "../assets/dashboardImages/podcastTwo.svg";
+import podcastThree from "../assets/dashboardImages/podcastThree.svg";
+import podcastFour from "../assets/dashboardImages/podcastFour.svg";
+import podcastFive from "../assets/dashboardImages/podcastFive.svg";
 
 const PodcastCardContainer = () => (
-  <section className='flex gap-[24px] overflow-x-auto no-scrollbar'>
-    <PodcastCard img={podcastOne} title="Mind Matters" category="Premium" status="online" />
-    <PodcastCard img={podcastOne} title="Tech Talk" category="Premium" status="offline" />
-    <PodcastCard img={podcastOne} title="History Quest" category="Premium" status="online" />
-    <PodcastCard img={podcastOne} title="Mystery Junction" category="Premium" status="online" />
-    <PodcastCard img={podcastOne} title="Creative Minds" category="Premium" status="offline" />
+  <section className="flex gap-[24px] overflow-x-auto no-scrollbar">
+    <PodcastCard
+      img={podcastOne}
+      title="Mind Matters"
+      category="Premium"
+      status="online"
+    />
+    <PodcastCard
+      img={podcastTwo}
+      title="Tech Talk"
+      category="Premium"
+      status="offline"
+    />
+    <PodcastCard
+      img={podcastThree}
+      title="History Quest"
+      category="Premium"
+      status="online"
+    />
+    <PodcastCard
+      img={podcastFour}
+      title="Mystery Junction"
+      category="Premium"
+      status="online"
+    />
+    <PodcastCard
+      img={podcastFive}
+      title="Creative Minds"
+      category="Premium"
+      status="offline"
+    />
   </section>
 );
 
 export default function PodcastContainer() {
   return (
-    <div className='flex flex-col gap-[20px]'>
-      <DashboardSectionHeader title="Podcasts" buttonLabels={['View All']} />
+    <div className="flex flex-col gap-[20px]">
+      <DashboardSectionHeader title="Podcasts" buttonLabels={["View All"]} />
       <PodcastCardContainer />
     </div>
   );
